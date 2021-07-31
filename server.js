34dcdb9ba53d7fe01100bff57ef47839499b10fb
Kitanga Nday <kitanga.nday@playsafesa.com>
2021-07-31 08:50:46 +0200

    Wrong port number

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
